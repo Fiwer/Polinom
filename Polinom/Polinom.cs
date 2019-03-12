ca2e3a699390880ba3f1d84adcaa10d25585b871
Sirazetdinov Dinar 11-811 <43180262+Fiwer@users.noreply.github.com>
2019-03-12 17:18:50 +0300

    Add files via upload

---
+++ Polinom/Polinom.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolinomTask
+{
+    public class Polinom
+    {
+        private List<Odnochlen> list = new List<Odnochlen>();
+
+        //Построение списка по полиному. Считывание с файла
+        public Polinom(string filename)
+        {
+            var polynomial = File.ReadAllLines(filename);
+            foreach (var row in polynomial)
+            {
+                var couple = row.Split(' ');
+                list.Add(new Odnochlen(int.Parse(couple[0]), int.Parse(couple[1])));//из каждой пары делаем одночлен
+            }
+        }
+
+        //Построение списка по полиному. Задается списком
+        public Polinom(List<Tuple<double,int>> list)
+        {
+            foreach(var pare in list)
+                this.list.Add(new Odnochlen(pare.Item1, pare.Item2));
+        }
+
+        //Номер коффициента в полиноме
+        public double this[int i]
+        {
+            get
+            {
+                if (i < 0 || i >= list.Count) throw new IndexOutOfRangeException();
+                return list[i].coefficient;
+            }
+
+            set
+            {
+                if (i < 0 || i >= list.Count) throw new IndexOutOfRangeException();
+                list[i].coefficient = value;
+            }
+        }
+
+        //Возврат строкового представления полинома.
+        public override string ToString()
+        {
+            var result = new StringBuilder();
+            Combine();
+            foreach (var odnoclen in list)
+            {
+                if (odnoclen == list[0]) result.Append(odnoclen.coefficient + "x^" + odnoclen.degree);
+                else
+                {
+                    result.Append(odnoclen.coefficient > 0 ? "+" : "");
+
+                    if (odnoclen.degree != 0)
+                        result.Append(odnoclen.coefficient + "x^" + odnoclen.degree);
+                    else result.Append(odnoclen.coefficient);
+                }
+            }
+            result.Append("= 0");
+            return result.ToString();
+        }
+
+        //Вставка монома coef*x^deg в полином.
+        public void Insert(int coef, int deg)
+        {
+            Sum(new Odnochlen(coef, deg));
+        }
+
+        //Приведение подобных членов в многочлене.
+        public void Combine()
+        {
+            foreach (var odnochlen in list)
+                if (odnochlen != list.Last() && odnochlen.degree == list.Last().degree)
+                {
+                    odnochlen.coefficient += list.Last().coefficient;
+                    list.Remove(list.Last());
+                    break;
+                }
+        }
+
+        //Удалить элемент с данным показателем степени.
+        public void Delete(int deg)
+        {
+            list = list.Where(z => z.degree != deg).ToList();
+            
+        }
+
+        //Прибавить к нашему полиному полином p.
+        //Привести подобные члены.
+        public void Sum(Odnochlen p)
+        {
+            list.Add(p);//добавление в конец списка
+            Combine();
+        }
+
+        //Взять производную у полинома.
+        public void Derivate()
+        {
+            list = list.Where(z => z.degree != 0).ToList();
+            list.ForEach(z => z.coefficient *= z.degree);
+            list.ForEach(z => z.degree--);
+        }
+
+        //Вычислить значение полинома в точке x, используя наиболее экономный способ (схема Горнера)
+        public int Value(int x)
+        {
+            Combine();
+            var CoefList = list.Select(z => z.coefficient).ToList();
+            var GornList = new List<double> {CoefList.First()};
+            //таблица из списка коофицентов и коофицентов по схеме горнера
+            CoefList.RemoveAt(0);
+            foreach(var coef in CoefList)
+                GornList.Add(x*GornList.Last()+coef);
+            return (int)Math.Round(GornList.Last());
+        }
+
+        //Удалить из списка все элементы с нечетными коэффициентами.
+        public void DeleteOdd()
+        {
+            list = list.Where(z => z.coefficient % 2 == 0).ToList();
+        }
+    }
+
+    public class Odnochlen
+    {
+        public int degree;
+        public double coefficient;
+        public Odnochlen(double coef, int gree)
+        {
+            degree = gree;
+            coefficient = coef;
+        }
+    }
+}
---
+++ Polinom/Tests.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PolinomTask;
+
+namespace ConsoleApp24
+{
+    [TestFixture]
+    class Tests
+    {
+        [Test]
+        public void GornerTest()
+        {
+            var test = new Polinom(new List<Tuple<double, int>>
+            {
+                new Tuple<double, int> (5, 4),
+                new Tuple<double, int> (5, 3),
+                new Tuple<double, int> (1, 2),
+                new Tuple<double, int> (-11, 0)
+            });
+            Assert.AreEqual(0, test.Value(1));
+        }
+        [Test]
+        public void GornerTest2()
+        {
+            var test = new Polinom(new List<Tuple<double, int>>
+            {
+                new Tuple<double, int> (1, 4),
+                new Tuple<double, int> (3, 3),
+                new Tuple<double, int> (4, 2),
+                new Tuple<double, int> (-5, 1),
+                new Tuple<double, int> (-47, 0)
+            });
+            Assert.AreEqual(4, test.Value(-3));
+        }
+        [Test]
+        public void GornerTest3()
+        {
+            var test = new Polinom(new List<Tuple<double, int>>
+            {
+                new Tuple<double, int> (3, 6),
+                new Tuple<double, int> (9, 5),
+                new Tuple<double, int> (-28, 4),
+                new Tuple<double, int> (6, 3),
+                new Tuple<double, int> (-30, 2),
+                new Tuple<double, int> (-30, 1),
+                new Tuple<double, int> (100, 0)
+            });
+            test.Value(2);
+            Assert.AreEqual(0, test.Value(-5));
+        }
+        [Test]
+        public void CombineTest()
+        {
+            var test = new Polinom(new List<Tuple<double, int>>
+            {
+                new Tuple<double, int> (5, 4),
+                new Tuple<double, int> (5, 3),
+                new Tuple<double, int> (1, 2),
+                new Tuple<double, int> (-11, 0),
+                new Tuple<double, int> (-11, 0)
+            });
+            test.Combine();
+            var test2 = new Polinom(new List<Tuple<double, int>>
+            {
+                new Tuple<double, int> (5, 4),
+                new Tuple<double, int> (5, 3),
+                new Tuple<double, int> (1, 2),
+                new Tuple<double, int> (-22, 0)
+            });
+
+            Assert.AreEqual(true, test2.ToString() == test.ToString());
+        }
+        [Test]
+        public void SumTest()
+        {
+            var test = new Polinom(new List<Tuple<double, int>>
+            {
+                new Tuple<double, int> (5, 4),
+                new Tuple<double, int> (5, 3),
+                new Tuple<double, int> (1, 2),
+                new Tuple<double, int> (-11, 0)
+            });
+            test.Sum(new Odnochlen(1, 4));
+            var test2 = new Polinom(new List<Tuple<double, int>>
+            {
+                new Tuple<double, int> (6, 4),
+                new Tuple<double, int> (5, 3),
+                new Tuple<double, int> (1, 2),
+                new Tuple<double, int> (-11, 0),
+                new Tuple<double, int> (0, 0)
+            });
+            Assert.AreEqual(true, test2.ToString() == test.ToString());
+        }
+        [Test]
+        public void ToStringTest()
+        {
+            var test = new Polinom(new List<Tuple<double, int>>
+            {
+                new Tuple<double, int> (5, 4),
+                new Tuple<double, int> (5, 3),
+                new Tuple<double, int> (1, 2),
+                new Tuple<double, int> (-11, 0)
+            });
+            Assert.AreEqual("5x^4+5x^3+1x^2-11= 0", test.ToString());
+        }
+        [Test]
+        public void FileReadTest()
+        {
+            var test = new Polinom(new List<Tuple<double, int>>
+            {
+                new Tuple<double, int> (5, 4),
+                new Tuple<double, int> (5, 3),
+                new Tuple<double, int> (1, 2),
+                new Tuple<double, int> (-11, 0)
+            });
+            
+            Assert.AreEqual(true, test.ToString() == test.ToString());
+        }
+        [Test]
+        public void IndexTest()
+        {
+            var test = new Polinom(new List<Tuple<double, int>>
+            {
+                new Tuple<double, int> (5, 4),
+                new Tuple<double, int> (5, 3),
+                new Tuple<double, int> (1, 2),
+                new Tuple<double, int> (-11, 0)
+            });
+            Assert.AreEqual(5, test[1]);
+        }
+        [Test]
+        public void InsertTest()
+        {
+            var test = new Polinom(new List<Tuple<double, int>>
+            {
+                new Tuple<double, int> (5, 4),
+                new Tuple<double, int> (5, 3),
+                new Tuple<double, int> (1, 2),
+                new Tuple<double, int> (-11, 0)
+            });
+            test.Insert(16, 4);
+            var test2 = new Polinom(new List<Tuple<double, int>>
+            {
+                new Tuple<double, int> (21, 4),
+                new Tuple<double, int> (5, 3),
+                new Tuple<double, int> (1, 2),
+                new Tuple<double, int> (-11, 0)
+            });
+            Assert.AreEqual(true, test.ToString() == test2.ToString());
+        }
+        [Test] 
+        public void DeleteTest()
+        {
+            var test = new Polinom(new List<Tuple<double, int>>
+            {
+                new Tuple<double, int> (5, 4),
+                new Tuple<double, int> (5, 3),
+                new Tuple<double, int> (1, 2),
+                new Tuple<double, int> (-11, 0)
+            });
+            test.Delete(4);
+            var test2 = new Polinom(new List<Tuple<double, int>>
+            {
+                new Tuple<double, int> (5, 3),
+                new Tuple<double, int> (1, 2),
+                new Tuple<double, int> (-11, 0)
+            });
+            Assert.AreEqual(true, test.ToString() == test2.ToString());
+        }
+        [Test]
+        public void DeleteOddTest()
+        {
+            var test = new Polinom(new List<Tuple<double, int>>
+            {
+                new Tuple<double, int> (5, 4),
+                new Tuple<double, int> (6, 3),
+                new Tuple<double, int> (1, 2),
+                new Tuple<double, int> (-12, 0)
+            });
+            test.DeleteOdd();
+            var test2 = new Polinom(new List<Tuple<double, int>>
+            {
+                new Tuple<double, int> (6, 3),
+                new Tuple<double, int> (-12, 0)
+            });
+            Assert.AreEqual(true, test.ToString() == test2.ToString());
+        }
+        [Test]
+        public void Derivate()
+        {
+            var test = new Polinom(new List<Tuple<double, int>>
+            {
+                new Tuple<double, int> (5, 4),
+                new Tuple<double, int> (6, 3),
+                new Tuple<double, int> (1, 2),
+                new Tuple<double, int> (-12, 0)
+            });
+            test.Derivate();
+            var test2 = new Polinom(new List<Tuple<double, int>>
+            {
+                new Tuple<double, int> (20, 3),
+                new Tuple<double, int> (18, 2),
+                new Tuple<double, int> (2, 1),
+            });
+            Assert.AreEqual(true, test.ToString() == test2.ToString());
+        }
+    }
+}
